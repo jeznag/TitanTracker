c8cd76874c1dbfad382223f3353331ed0fc29465
Jeremy Nagel <jeremymnagel@gmail.com>
2017-06-04 14:00:44 +0800

    Add highlighting for consistent vs inconsistent habits

--- Components/Track.js
+++ Components/Track.js
@@ -1,10 +1,5 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  ListView
-} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, ListView} from 'react-native';
 import {connect} from 'react-redux';
 import Accordion from 'react-native-collapsible/Accordion';
 import NumberInput from './inputs/Number';
@@ -14,26 +9,33 @@ import CheckInActionCreator from '../redux/action-creators/checkin';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingLeft: 20,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  accordionContent: {
+    paddingLeft: 20,
+  },
+  headerText: {
     fontSize: 20,
-    textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  consistentCheckin: {
+    backgroundColor: 'green'
+  },
+  somewhatInconsistentCheckin: {
+    backgroundColor: 'orange'
+  },
+  reallyInconsistentCheckin: {
+    backgroundColor: 'red'
   },
 });
 
 const timeBlocks = ['morning', 'midday', 'afternoon', 'evening'];
 
 function getHabitsForTimeBlock(habitData, timeBlock) {
-  return habitData.filter((habit) => habit.timeBlock === timeBlock);
+  return habitData.filter(habit => habit.timeBlock === timeBlock);
 }
 
 function getHabitComponents(habits) {
@@ -43,15 +45,43 @@ function getHabitComponents(habits) {
 function getHabitComponent(handleCheckin, habit) {
   if (habit.type === 'number') {
     return (
-      <NumberInput habit={habit} handleCheckin={handleCheckin} />
+      <NumberInput
+        habit={habit}
+        handleCheckin={handleCheckin}
+        key={habit.habitID}
+        style={getBackgroundColourForHabits([habit])}
+      />
     );
   } else if (habit.type === 'boolean') {
     return (
       <BooleanInput
+        key={habit.habitID}
         habit={habit}
         handleCheckin={handleCheckin}
+        style={getBackgroundColourForHabits([habit])}
       />
     );
+  }
+}
+
+function getBackgroundColourForHabits(habits) {
+  const consistentCheckinStyle = styles.consistentCheckin;
+  const somewhatInconsistentCheckinStyle = styles.inconsistentCheckin;
+
+  const DAY_LENGTH = 24 * 60 * 60 * 1000;
+  const totalDaysSinceGoodCheckin = habits.reduce((totalDays, habit) => {
+    const daysSinceGoodCheckin = (new Date() - new Date(habit.lastCheckin)) / DAY_LENGTH;
+    return totalDays + daysSinceGoodCheckin;
+  }, 0);
+
+  if (totalDaysSinceGoodCheckin === 0) {
+    return null;
+  } else if (totalDaysSinceGoodCheckin < 1) {
+    return styles.consistentCheckin;
+  } else if (totalDaysSinceGoodCheckin < 5) {
+    return styles.somewhatInconsistentCheckin;
+  } else {
+    return styles.reallyInconsistentCheckin;
   }
 }
 
@@ -59,28 +89,34 @@ function renderHeader(habitData, timeBlock) {
   const habitsForThisTimeBlock = getHabitsForTimeBlock(habitData, timeBlock);
   const completedHabits = 0;
   const heading = `${timeBlock} (${completedHabits} / ${habitsForThisTimeBlock.length})`;
-  return (<View style={styles.header}>
-    <Text style={styles.headerText}>{heading}</Text>
-  </View>);
+  return (
+    <View style={[styles.header, getBackgroundColourForHabits(habitsForThisTimeBlock)]}>
+      <Text style={styles.headerText}>{heading}</Text>
+    </View>
+  );
 }
 
 function renderAccordionContent(habitData, handleCheckin, timeBlock) {
   const habitsForThisTimeBlock = getHabitsForTimeBlock(habitData, timeBlock);
-  const ds = new ListView.DataSource({rowHasChanged: () => false});
-  const data = ds.cloneWithRows(habitsForThisTimeBlock);
-  return (<ListView
-    dataSource={data}
-    enableEmptySections={true}
-    renderRow={getHabitComponent.bind(null, handleCheckin)}
-  />);
+  return (
+    <View style={styles.header}>
+      {habitsForThisTimeBlock.map(getHabitComponent.bind(null, handleCheckin))}
+    </View>
+  );
 }
 
 function getTimeBlocks(habitData, handleCheckin) {
-  return (<Accordion
-    sections={timeBlocks}
-    renderHeader={renderHeader.bind(null, habitData)}
-    renderContent={renderAccordionContent.bind(null, habitData, handleCheckin)}
-  />);
+  return (
+    <Accordion
+      sections={timeBlocks}
+      renderHeader={renderHeader.bind(null, habitData)}
+      renderContent={renderAccordionContent.bind(
+        null,
+        habitData,
+        handleCheckin
+      )}
+    />
+  );
 }
 
 class TrackScreen extends Component {
@@ -96,7 +132,7 @@ class TrackScreen extends Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     handleCheckin(habit, value) {
       dispatch(CheckInActionCreator.checkIn(habit, value));
